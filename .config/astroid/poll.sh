fd123d798083be249031a2f707dacf8727d6d357
Ashymad <czilukim@o2.pl>
2017-10-04 23:00:38 +0200

    added new email account

--- .config/astroid/poll.sh
+++ .config/astroid/poll.sh
@@ -17,6 +17,7 @@ fi
 
 notmuch search --output=files tag:deleted and tag:o2 and not folder:o2/Trash | xargs -I {} mv -f "{}" $MAILDIR/o2/Trash/cur/
 notmuch search --output=files tag:deleted and tag:freeos and not folder:freeos/Trash | xargs -I {} mv -f "{}" $MAILDIR/freeos/Trash/cur/
+notmuch search --output=files tag:deleted and tag:agh and not folder:agh/Trash | xargs -I {} mv -f "{}" $MAILDIR/agh/Trash/cur/
 
 offlineimap
 notmuch new
@@ -24,11 +25,12 @@ notmuch new
 notmuch tag --batch <<EOF
 	+o2 tag:new and path:"o2/**"
 	+freeos tag:new and path:"freeos/**"
-	+deleted tag:new and folder:o2/Trash or folder:freeos/Trash
-	+sent tag:new and folder:o2/Sent or folder:freeos/Sent or folder:"freeos/Sent Items"
-	+spam tag:new and folder:o2/Spam or folder:freeos/Spam or folder:freeos/Junk
-	+inbox tag:new and folder:o2/INBOX or folder:freeos/INBOX
-	+archive tag:new and folder:o2/INBOX.Archive or folder:freeos/Archive
-	+draft tag:new and folder:o2/Drafts or folder:freeos/Drafts
+	+agh tag:new and path:"agh/**"
+	+deleted tag:new and folder:o2/Trash or folder:freeos/Trash or folder:agh/Trash
+	+sent tag:new and folder:o2/Sent or folder:freeos/Sent or folder:"freeos/Sent Items" or folder:agh/Sent
+	+spam tag:new and folder:o2/Spam or folder:freeos/Spam or folder:freeos/Junk or folder:agh/Spam
+	+inbox tag:new and folder:o2/INBOX or folder:freeos/INBOX or folder:agh/INBOX
+	+archive tag:new and folder:o2/INBOX.Archive or folder:freeos/Archive or folder:agh/Archives
+	+draft tag:new and folder:o2/Drafts or folder:freeos/Drafts or folder:agh/Drafts
 	-new tag:new
 EOF
