c324b8360c2e10804894ec70d2203371483b1954
Ashymad <czilukim@o2.pl>
2017-10-14 23:36:54 +0200

    Merge changes from notwobug branch

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,9 +1,22 @@
 import keepasshttp
+import sys, os
+
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
 
 session = keepasshttp.start('offlineimap')
 
 def get_password( str ):
-    return session.getLogins( str )[0]['Password'].value
+    blockPrint()
+    passw = session.getLogins( str )[0]['Password'].value
+    enablePrint()
+    return passw
 
 def get_username( str ):
-    return session.getLogins( str )[0]['Login']
+    blockPrint()
+    login = session.getLogins( str )[0]['Login']
+    enablePrint()
+    return login
