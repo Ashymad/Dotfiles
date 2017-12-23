69151851bed3bce3538713fb10abd5f9f7a4b900
Ashymad <czilukim@o2.pl>
2017-12-23 16:20:06 +0100

    Merge changes from notwobug branch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 #!/bin/env fish
 
 alias ec "emacsclient -a \"\""
-alias bash "bash --norc"
+alias fcd 'cd (fd -I -L -d 8 -t d . ~ | fzf)'
+alias bash 'env FISH_RAMP_DISABLE=1 bash'
 alias psgr 'ps aux | grep'
+
