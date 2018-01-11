8d187931645e4873a5e9ffc2e51efea116e10a78
Ashymad <czilukim@o2.pl>
2018-01-11 21:38:00 +0100

    Add reverse history search

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,12 @@ alias fcd 'cd (fd -I -L -d 8 -t d . ~ | fzf)'
 alias bash 'env FISH_RAMP_DISABLE=1 bash'
 alias psgr 'ps aux | grep'
 
+function reverse_search
+	commandline -r (grep '^- cmd: '  ~/.local/share/fish/fish_history | cut -c 8- | fzf)
+end
+
+
+function fish_user_keybindings
+	bind \cr reverse_search
+end
+
