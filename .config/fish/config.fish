638d3b45f19f77f0edd2c1dcce2ab43871b75ca8
Ashymad <czilukim@o2.pl>
2018-01-29 12:07:00 +0100

    vim alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,8 @@ alias ec "emacsclient -a \"\""
 alias fcd 'cd (fd -I -L -d 8 -t d . ~ | fzf)'
 alias bash 'env FISH_RAMP_DISABLE=1 bash'
 alias psgr 'ps aux | grep'
+alias vim nvim
+
 source /usr/share/autojump/autojump.fish
 
 
