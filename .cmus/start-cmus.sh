b0a7d0706d62f2258c41082550b6851098e1f271
Ashymad <czilukim@o2.pl>
2018-01-29 12:04:43 +0100

    Switch to CAVA GUI

--- .cmus/start-cmus.sh
+++ .cmus/start-cmus.sh
@@ -3,4 +3,4 @@
 i3-msg "workspace 4; append_layout /home/shyman/.i3/cmus.json"
 nohup geeqie -t > /dev/null &
 nohup urxvt -e "cmus" > /dev/null &
-nohup urxvt -fn "xft:monaco:size=8" -e cava > /dev/null &
+nohup cava > /dev/null &
