3dd2930c24bf0c7120649ef236eaec513d585ff8
Ashymad <czilukim@o2.pl>
2018-02-20 16:01:59 +0100

    Cleanup config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 #!/bin/env fish
 
-alias ec "emacsclient -a \"\""
-alias fcd 'cd (fd -I -L -d 8 -t d . ~ | fzf)'
-alias bash 'env FISH_RAMP_DISABLE=1 bash'
-alias psgr 'ps aux | grep'
+alias ec	"emacsclient -a \"\""
+alias fcd	'cd (fd -I -L -d 8 -t d . ~ | fzf)'
+alias bash	'env FISH_RAMP_DISABLE=1 bash'
+alias psgr	'ps aux | grep'
+alias vim	'nvim'
 
