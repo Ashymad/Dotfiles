157278ffc66e51a25bd4c0ca40e8ab21b04faf28
Ashymad <czilukim@o2.pl>
2018-02-24 12:14:49 +0100

    Change font

--- .st/config.h
+++ .st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Monofur:size=14:antialias=true:autohint=true";
+char font[] = "Monofur:size=16:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
