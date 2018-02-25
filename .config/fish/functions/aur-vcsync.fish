da69f4dc17570bd58100e47de0e2500f9fd511a2
Ashymad <czilukim@o2.pl>
2018-02-25 16:06:15 +0100

    Repair messages

--- .config/fish/functions/aur-vcsync.fish
+++ .config/fish/functions/aur-vcsync.fish
@@ -2,14 +2,14 @@ function aur-vcsync
 	set vcs_list git hg cvs svn bzr
 	echo -n "" > /tmp/vcs.tmp
 	set_color -o
-	echo ==> Checking newest commit of repositories...
+	echo "==> Checking newest commit of repositories..."
 	for vcs in $vcs_list
 		set repos ~/.cache/aurutils/sync/*-$vcs
 		for repo in $repos
 			aur srcver $repo >> /tmp/vcs.tmp 
 		end
 	end
-	echo ==> Comparing to locally installed versions...
+	echo "==> Comparing to locally installed versions..."
 	set updates (aur vercmp -p /tmp/vcs.tmp -d custom-aur)
 	for update in $updates
 		echo Updating $update...
