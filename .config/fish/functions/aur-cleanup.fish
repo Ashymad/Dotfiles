01f6447471c57eed9919e3925bde35e786cb3f55
Ashymad <czilukim@o2.pl>
2018-03-02 21:08:36 +0100

    aur cleanup function

---
+++ .config/fish/functions/aur-cleanup.fish
@@ -0,0 +1,31 @@
+function aur-cleanup
+	set_color -o blue
+	echo -n ":: "
+	set_color -o white
+	echo "Cleaning pacman cache..."
+	set_color normal
+	sudo pacman -Sc
+	set_color -o blue
+	echo -n ":: "
+	set_color -o white
+	echo "Updating custom repositories..."
+	set_color normal
+	for repo in /var/cache/pacman/*custom*
+		cd $repo
+		repose -zvf (basename $repo)
+	end
+	sudo pacman -Sy
+	set_color -o blue
+	echo -n ":: "
+	set_color -o white
+	echo "Cleaning aur sync cache..."
+	set_color normal
+	cd ~/.cache/aurutils/sync
+	for dir in *
+		set pkgname (bash -c "source $dir/PKGBUILD;"'echo $pkgname')
+		if not pacman -Qi $pkgname >/dev/null 2>/dev/null
+			rm -rvf -- $dir
+		end
+	end
+end
+
