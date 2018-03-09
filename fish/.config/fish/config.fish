70e80d05bbe20fd66fd597861f0a7d4702681cb7
Ashymad <czilukim@o2.pl>
2018-03-09 23:37:33 +0100

    Wine aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,4 +8,12 @@ alias bash 'env FISH_RAMP_DISABLE=1 bash'
 alias psgr 'ps aux | grep'
 alias vim nvim
 
+alias wine64 'env WINEPREFIX=/home/shyman/.wine64 WINEARCH=win64 wine'
+alias wine64cfg 'env WINEPREFIX=/home/shyman/.wine64 WINEARCH=win64 winecfg'
+alias wine64tricks 'env WINEPREFIX=/home/shyman/.wine64 WINEARCH=win64 winetricks'
+
+alias wine32 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 wine'
+alias wine32cfg 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 winecfg'
+alias wine32tricks 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 winetricks'
+
 source /usr/share/autojump/autojump.fish
