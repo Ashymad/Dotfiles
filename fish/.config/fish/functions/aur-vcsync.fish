d6e83ee66c8636cad3dd1ca55677257e041d780e
Ashymad <czilukim@o2.pl>
2018-04-11 16:16:46 +0200

    Improve vcsync

--- fish/.config/fish/functions/aur-vcsync.fish
+++ fish/.config/fish/functions/aur-vcsync.fish
@@ -1,16 +1,14 @@
 function aur-vcsync
 	source ~/.config/fish/utils/pacmsg.fish
 	set vcs_list git hg cvs svn bzr
-	echo -n "" > /tmp/vcs.tmp
 	msg1 "Checking newest commit of repositories..."
-	for vcs in $vcs_list
-		set repos ~/.cache/aurutils/sync/*-$vcs
-		for repo in $repos
-			aur srcver $repo >> /tmp/vcs.tmp 
-		end
-	end
+	aur srcver ~/.cache/aurutils/sync/*-$vcs_list > /tmp/vcs.tmp 
 	msg1 "Comparing to locally installed versions..."
 	set updates (aur vercmp -p /tmp/vcs.tmp -d custom-aur)
+	if test -z "$updates"
+		msg3 "There is nothing to do"
+		exit
+	end
 	for update in $updates
 		echo Updating $update...
 		aur sync --no-ver \
