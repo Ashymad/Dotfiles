727d3674aebe2025eda17a3dd66653ca4e55282a
Ashymad <czilukim@o2.pl>
2018-07-02 18:59:16 +0200

    Fix slow recovery after bad command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,4 +16,8 @@ alias wine32 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 wine'
 alias wine32cfg 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 winecfg'
 alias wine32tricks 'env WINEPREFIX=/home/shyman/.wine32 WINEARCH=win32 winetricks'
 
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    __fish_default_command_not_found_handler $argv[1]
+end
+
 source /usr/share/autojump/autojump.fish
