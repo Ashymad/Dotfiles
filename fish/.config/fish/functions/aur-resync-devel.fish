eb76c791441a558abc04c47571868fc84c3822e7
Ashymad <czilukim@o2.pl>
2018-08-25 17:42:57 +0200

    New version

--- fish/.config/fish/functions/aur-resync-devel.fish
+++ fish/.config/fish/functions/aur-resync-devel.fish
@@ -1,4 +1,4 @@
 function aur-resync-devel
-	aur sync --list | cut -f2 | grep -E -- "-$AURVCS" | aur sync --no-ver --print -
+	aur sync --list | cut -f2 | grep -E -- "-$AURVCS" | xargs aur sync --no-ver --print
 end
 
