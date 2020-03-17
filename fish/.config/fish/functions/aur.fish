8de0cd3947fea45c9be1b46c84faa96060b3fb80
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-03-17 22:12:59 +0100

    fish: ugprade aur-cleanup and aur + add aur-vercmp-devel and rollback

--- fish/.config/fish/functions/aur.fish
+++ fish/.config/fish/functions/aur.fish
@@ -1,9 +1,11 @@
 #!/usr/bin/env fish
 function aur
-	if test -f "$HOME/.config/fish/functions/aur-$argv[1].fish"
-		eval "aur-$argv[1]" $argv[2..-1]
-	else
-		/usr/bin/aur $argv
-	end
+    if test -f "$HOME/.config/fish/functions/aur-$argv[1].fish"
+        eval "aur-$argv[1]" $argv[2..-1]
+    else if test -f "$HOME/.config/fish/functions/aur-$argv[1].sh"
+        bash "$HOME/.config/fish/functions/aur-$argv[1].sh"
+    else
+        /usr/bin/aur $argv
+    end
 end
 
