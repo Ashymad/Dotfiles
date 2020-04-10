df60f0ea8718f56bf9d523a02b878d87e3fff718
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-04-10 18:22:10 +0200

    polybar: global theme + cmus fix

--- polybar/.config/polybar/cmus.sh
+++ polybar/.config/polybar/cmus.sh
@@ -4,9 +4,9 @@ ARTIST=$(grep 'tag artist ' /tmp/cmus.status | tail -c +12)
 STATE=$(grep 'status ' /tmp/cmus.status | tail -c +8)
 
 if [ $STATE = 'playing' ]; then
-    echo "▶ $ARTIST - $TITLE"
+    echo " $ARTIST - $TITLE"
 elif [ $STATE = 'paused' ]; then
-    echo "⏸ $ARTIST - $TITLE"
+    echo " $ARTIST - $TITLE"
 else
-    echo "■"
+    echo ""
 fi
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 killall polybar
 #env MONITOR=$(polybar -m|head -1|sed -e 's/:.*$//g')
-polybar example
+exec polybar -r example
