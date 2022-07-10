87f72a4a0addb7bf6b27977641c6052cce18d4ea
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2022-07-10 13:54:50 +0200

    Update update-devel

--- fish/.config/fish/functions/aur-update-devel.fish
+++ fish/.config/fish/functions/aur-update-devel.fish
@@ -1,5 +1,5 @@
 function aur-update-devel
-    set packages (aur vercmp-devel | cut -d: -f1)
+    set packages (aur vercmp-devel | cut -d' ' -f1)
     if not test -z "$packages"
 	aur sync $packages --no-ver-argv
     end
