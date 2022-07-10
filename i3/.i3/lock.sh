158d6cc1e11dfeb68716c6cec4b0a39d0d319122
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2022-07-10 13:35:26 +0200

    Update i3

--- i3/.i3/lock.sh
+++ i3/.i3/lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export XSECURELOCK_FONT="Fira Mono"
-export XSECURELOCK_SAVER=saver_mpv
+export XSECURELOCK_FONT="-misc-liberation mono-medium-r-normal--0-0-0-0-m-0-iso10646-1"
+export XSECURELOCK_SAVER=$HOME/.local/share/xsecurelock/helpers/saver_mpv
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
 export XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/.i3/BG.png"
 export XSECURELOCK_IMAGE_DURATION_SECONDS=inf
